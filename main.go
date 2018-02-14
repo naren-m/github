a3af8f8c01274c5b1234e29e3764682598fd0997
Naren Mudivarthy <nmudivar@cisco.com>
2018-02-13 18:40:25 -0800

    Inital commit for git cli

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/google/go-github/github"
+	"github.com/naren-m/github-cli/services"
+)
+
+func main() {
+	ctx := context.Background()
+	client := github.NewClient(nil)
+
+	githubAPI := services.NewGithub(ctx, client.Repositories)
+	repo, err := githubAPI.GetPackageRepoInfo("Golang-Coach", "Lessons")
+	//	repo, _, err := client.Repositories.Get(ctx, "naren-m", "dotfiles")
+
+	if err != nil {
+		fmt.Printf("Problem in getting repository information %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("%+v\n", repo)
+
+}
