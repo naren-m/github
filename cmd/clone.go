1026e76b08b9273a46358b061c65b2f333fd5167
Naren Mudivarthy <nmudivar@cisco.com>
2018-02-17 17:42:15 -0800

    added gitignore

--- cmd/clone.go
+++ cmd/clone.go
@@ -4,20 +4,18 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // cloneCmd represents the clone command
 var cloneCmd = &cobra.Command{
 	Use:   "clone",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Clone a Git repository",
+	Long:  `Clone a Git repository`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("clone called")
+		viper.ReadInConfig()
+		fmt.Println(viper.GetString("url"))
 	},
 }
 
--- cmd/root.go
+++ cmd/root.go
@@ -9,7 +9,12 @@ import (
 	"github.com/spf13/viper"
 )
 
-var cfgFile string
+var (
+	url string
+	// ConfigFile is $HOME/.tatcli/config.json per default
+	// contains user, password and url of tat
+	cfgFile string
+)
 
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
@@ -33,11 +38,10 @@ func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
-	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.github.yaml)")
+	// RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.github.yaml)")
+	RootCmd.PersistentFlags().StringVarP(&url, "url", "", "", "URL Tat Engine, facultative if you have a $HOME/.github/config.json file")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	viper.BindPFlag("url", RootCmd.PersistentFlags().Lookup("url"))
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -45,6 +49,7 @@ func initConfig() {
 	if cfgFile != "" {
 		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
+		fmt.Println("non empty config")
 	} else {
 		// Find home directory.
 		home, err := homedir.Dir()
@@ -55,6 +60,7 @@ func initConfig() {
 
 		// Search config in home directory with name ".github" (without extension).
 		viper.AddConfigPath(home)
+		fmt.Println("Hone", home)
 		viper.SetConfigName(".github")
 	}
 
@@ -64,4 +70,5 @@ func initConfig() {
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
+
 }
